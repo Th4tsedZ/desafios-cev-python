3c248f9722037959f611642d8ce39b6d609b92f3
Francisco Edenilson <edenilson.victoralves@outlook.com>
2018-07-24 23:05:32 -0300

    rm redundancies
    Removing some redundancies in attribution of the bigger and smaller weights.

--- desafio055.py
+++ desafio055.py
@@ -1,9 +1,6 @@
 '''
 Faça um programa que leia o peso de cinco pessoas e mostre qual foi o maior e o menor peso.
 '''
-maior = 0
-menor = 0
-maior = menor = 0
 maior = menor = 0
 for c in range(1, 6):
     peso = float(input('Peso da {}° pessoa: '.format(c)))
