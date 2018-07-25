8936813786a5e36178c94dc39bcf951ef549e7c3
Francisco Edenilson <edenilson.victoralves@outlook.com>
2018-07-24 22:59:18 -0300

    Statement 055
    Addtion of statement in the challenge.

--- desafio055.py
+++ desafio055.py
@@ -1,10 +1,16 @@
+<<<<<<< HEAD
+'''
+Faça um programa que leia o peso de cinco pessoas e mostre qual foi o maior e o menor peso.
+'''
 maior = 0
 menor = 0
+=======
+maior = menor = 0
+>>>>>>> 1d5ff6a5dd62bd192391ed400e59ec834c439411
 for c in range(1, 6):
     peso = float(input('Peso da {}° pessoa: '.format(c)))
     if c == 1:
-        maior = peso
-        menor = peso
+        maior = menor = peso
     else:
         if peso > maior:
             maior = peso
